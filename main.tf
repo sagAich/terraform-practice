b26ad93544ae0eacdda167e6eee079e242cdf7cc
Sagar Aich <sagaraich01@gmail.com>
2026-06-30 22:37:43 +0530

    some modification on main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,9 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-    }
-  }
-}
-
 provider "aws" {
   region  = "ap-south-1"
   profile = "terraform-lab"
 }
 
-data "aws_caller_identity" "current" {}
-
-output "authenticated_arn" {
-  value = data.aws_caller_identity.current.arn
+resource "aws_instance" "example"{
+  ami = "ami-01a00762f46d584a1" 
+  instance_type = "t2.micro"
 }
